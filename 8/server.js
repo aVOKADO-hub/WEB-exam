04c90fd110d8ea55c2d0d4d2c261e3ca57d5845b
aVOKADO-hub <polsasha735@gmail.com>
2024-12-12 23:54:07 +0200

    add 8 task

---
+++ 8/server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        const filePath = path.join(__dirname, '/public/index.html');
+        fs.readFile(filePath, (err, content) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content, 'utf8');
+            }
+        });
+    } else {
+        res.writeHead(404);
+        res.end('Not Found');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
